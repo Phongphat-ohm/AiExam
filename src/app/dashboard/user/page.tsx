b849a93a9d56630c6e2af64d574f53be32a0ef5c
Phongphat <paspat2121@gmail.com>
2024-12-30 11:59:22 +0700

    แก้ error ไม่พบไฟล์ user

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/Dashboard/Layout";
-import UserIndex from "@/components/Dashboard/user/index";
+import UserIndex from "@/components/Dashboard/User/index";
 
 export default function UserDashboard() {
     return (
